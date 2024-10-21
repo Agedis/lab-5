03b05607f61bd3a220b51c5c8f92463b8b251a9e
ShanayaGoel <shanayagoel777@gmail.com>
2024-10-21 12:44:15 -0400

    hhj

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -5,8 +5,19 @@ package use_case.logout;
  */
 public class LogoutInputData {
 
+    private String username;
+
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        // TODO: save the current username in an instance variable and add a getter. done I think
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
 }
